7c4216e4f8f5164f40750276308d318c9c7b068c
Gamze Altınelli <gamseacar@windowslive.com>
2022-02-01 15:25:00 +0300

    Add project files.

---
+++ Karesi2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Karesi2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write(" Sayı: ");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Gidiğiiz sayının karesi: "+(sayi*sayi));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SecondConsoleApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecondConsoleApp
+{
+    class Program
+    {
+        //1.Adım:Başla
+        //2.Adım:Kullanıcıdan sayi verisi al
+        //3.karesi değişken ata
+        //4.karesi=sayi^2 olsun
+        //5.karesi ekrana yaz
+        //6.Bitir
+        static void Main(string[] args)
+        {
+            Console.Write("Bir sayi giriniz: ");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+            float karesi = sayi*sayi;
+            Console.Write("Girilen sayının karesi: "+karesi);
+            Console.ReadKey();
+
+        }
+    }
+}
